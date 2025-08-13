8d120d4870f795fe59ae52780a28aaaaa4877bad
Nicolas Lima <nlimadasilvabrito@gmail.com>
2025-08-13 09:20:25 -0300

    feat: implement Home component with template, styles, and unit tests

---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,47 @@
+import { Component, effect, Inject, OnInit, signal } from '@angular/core';
+import { UserTable } from '../../components/user-table/user-table';
+import { UserService } from '../../services/user-service/user-service';
+import { LoadingService } from '../../services/loading-service/loading-service';
+import { FormatService } from '../../services/format-service/format-service';
+import { ButtonGroup } from '../../components/button-group/button-group';
+import { AnchorItem } from '../../interfaces/anchor-item';
+
+@Component({
+  selector: 'app-home',
+  imports: [UserTable, ButtonGroup],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home implements OnInit {
+
+  constructor(
+    @Inject(UserService) public userService: UserService, 
+    @Inject(LoadingService) private loadingService: LoadingService,
+    @Inject(FormatService) private formatService: FormatService
+  ) {
+      effect(() => {
+        if (!this.searched()) {
+          return;
+        }
+        
+        this.showEmptyMessage.set(this.userService.users().length === 0);
+      });
+  }
+
+  buttons : AnchorItem[] = [{
+    type: "anchor",
+    path: "/users/create",
+    text: "Criar usuário"
+  }]
+  
+  searched = signal<boolean>(false);
+  showEmptyMessage = signal<boolean>(false);
+  
+  ngOnInit(): void {
+    this.userService.fetchUsers(() => {
+      this.searched.set(true);
+    });
+    
+  }
+  
+}

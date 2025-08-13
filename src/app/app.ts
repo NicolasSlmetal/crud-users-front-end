cb418e8985ea52885128e8300541e0dcb6f802e9
Nicolas Lima <nlimadasilvabrito@gmail.com>
2025-08-13 09:23:59 -0300

    feat: add app container and loading components to main application structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component, signal } from '@angular/core';
+import { Component, Inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoadingModal } from './components/loading-modal/loading-modal';
+import { LoadingService } from './services/loading-service/loading-service';
+import { ToastContainer } from './components/toast-container/toast-container';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoadingModal, ToastContainer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('front-end');
+  constructor(@Inject(LoadingService) private loadingService : LoadingService) {
+
+  }
+
+  public isLoading() {
+    return this.loadingService.isLoading();
+  }
+
+
 }

abf88ea52e2807e7ba082e6ff520267dd51b4a07
Nicolas Lima <nlimadasilvabrito@gmail.com>
2025-08-13 09:08:10 -0300

    feat: implement AddressCard component with HTML, CSS, and tests

---
+++ src/app/components/address-card/address-card.ts
@@ -0,0 +1,44 @@
+import { Component, EventEmitter, input, OnInit, Output, signal } from '@angular/core';
+import { ButtonGroup } from '../button-group/button-group';
+import { ButtonItem } from '../../interfaces/button-item';
+import { AnchorItem } from '../../interfaces/anchor-item';
+import { AddressModel } from '../../models/addresses/address-model';
+
+@Component({
+  selector: 'app-address-card',
+  imports: [ButtonGroup],
+  templateUrl: './address-card.html',
+  styleUrl: './address-card.css'
+})
+export class AddressCard implements OnInit {
+
+  ngOnInit(): void {
+      this.buttons.set(
+        [
+          {
+            type: "anchor",
+            text: "Editar",
+            path: `addresses/${this.address().id}/edit`,
+          }, 
+          {
+            type: "button",
+            text: "Deletar",
+            extraClasses: ["danger"],
+            callback: () => this.sendToConfirmDelete()
+          }
+        ]
+      )
+  }
+  
+  address = input.required<AddressModel>();
+  
+  @Output("clickToDelete") clickToDelete = new EventEmitter<AddressModel>();
+
+  public sendToConfirmDelete() {
+    this.clickToDelete.emit(this.address());
+  }
+
+  buttons  = signal<Array<ButtonItem | AnchorItem>>([]);
+
+
+}

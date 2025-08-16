681a240e8176f63d8d0952859d073363e2eeb137
Nicolas Lima <nlimadasilvabrito@gmail.com>
2025-08-16 15:16:05 -0300

    feat: create phone pipe to phone formatting

---
+++ src/app/pipes/phone-pipe/phone-pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'phone'
+})
+export class PhonePipe implements PipeTransform {
+
+  transform(value: string): string {
+    return value.replace(/(\d{2})(\d{4,5})(\d{4})/, "($1) $2-$3");
+  }
+
+}
